686c3c559e0da5ca9436a474c5943e1e2c69ef8a
MIG2558 <113391782+MIG2558@users.noreply.github.com>
2024-02-08 21:27:25 -0300

    Create aa.lua

---
+++ aa.lua
@@ -0,0 +1,12 @@
+print('digite sua idade')
+age=io.read()
+print('Essa é sua idade?')
+print(tonumber(age)) 
+if age >= (tostring(45)) then
+       print("Nossa ta velho em") 
+       print('tchau kk')
+end
+if age <= (tostring(45)) then
+       print("Parabéns, vc tá jovem!")
+       print("Tchau kk")
+end
