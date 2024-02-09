083f7146ef99abb596edd68c0fff55d01020a7ef
Miguel Lopes <113391782+MIG2558@users.noreply.github.com>
2024-02-09 13:12:51 -0300

    Answer_1.lua

---
+++ Answer_1.lua
@@ -0,0 +1,9 @@
+print("Você gosta de programação?")
+print("1 = sim, 0 = nao")
+answer=io.read("*number")
+if answer == 0 then
+       print(":(")
+end
+if answer == 1 then
+       print("Ótimo!!!!!")
+end
