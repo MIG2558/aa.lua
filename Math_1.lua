abd5d839b894e4ef899cee533505ef066faa7ac9
Miguel Lopes <113391782+MIG2558@users.noreply.github.com>
2024-02-10 10:45:38 -0300

    Math_1.lua

---
+++ Math_1.lua
@@ -0,0 +1,16 @@
+print("Qual a raiz quadrada de 81?")
+R=io.read("*number")
+if R == 9 then 
+       print("Acertou!!!!!!") 
+end
+if R <= 8 then 
+       print("ERROU!!") 
+end
+if R >= 10 then 
+       print("ERROU!!") 
+end
+print("Agr como é 9 ao quadrado?")
+R2=io.read("*number")
+if R2 == 81 then 
+       print("Acertou miseravi")
+end
