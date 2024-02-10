12a8a5e865ee7bdb4e14d819b25a7903cdc368f5
Miguel Lopes <113391782+MIG2558@users.noreply.github.com>
2024-02-10 20:06:15 -0300

    Jokecode.lua

---
+++ Jokecode.lua
@@ -0,0 +1,13 @@
+print("2 + 2 = ? Resolva")
+i=io.read("*number")
+if i == 4 then
+       print("ERROU!!")
+end
+if i == 22 then
+       print("AHHAHAHA")
+       print("ACERTOU!!")
+end
+--[[ Esse código é só uma piada
+ ahhaha
+tchauu!
+--]]
