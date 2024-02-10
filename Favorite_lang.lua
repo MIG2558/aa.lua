c17d56160f3bc26476476da88dcb9fe1d857d889
Miguel Lopes <113391782+MIG2558@users.noreply.github.com>
2024-02-10 11:25:00 -0300

    Favorite_lang.lua

---
+++ Favorite_lang.lua
@@ -0,0 +1,14 @@
+print("Qual sua linguagem de programação favorita entre essas 4: Python, Lua, C#, C++?")
+v=io.read()
+if v == "Lua" then
+       print("Eu te amo kk")
+end
+if v == "Python" then
+       print("Ok!!")
+end
+if v == "C#" then
+       print("Ok!!")
+end
+if v == "C++" then
+       print("Ok!!")
+end
