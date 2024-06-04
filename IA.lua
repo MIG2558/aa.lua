15e4f5aa49fd44a2fab3ea27eddeaf3aa9467e6e
Miguel Lopes <113391782+MIG2558@users.noreply.github.com>
2024-06-04 13:21:00 -0300

    IA.lua

---
+++ IA.lua
@@ -0,0 +1,24 @@
+print("Qual é seu nome?")
+nome=io.read()
+if nome == "Miguel" then
+    print("Nossa! Vc tem o mesmo nome do que eu!")
+end
+print("Bom...")
+nome2=io.read()
+if nome2 == "Oq?" then
+    print("É que um preguiçoso me programou...")
+end
+ata=io.read()
+if ata == "ata" then
+    print("Pois é, sou uma IA lixo, n sou?")
+end
+ata2=io.read()
+if ata2 == "Nossa... Vc n é um lixo!" then
+    print("Obg! :)")
+end
+obg=io.read
+if obg == "de nada! :)" then
+    print(":)")
+end
+print("tchauu! :)")
+print("Gostei de vc! >:D")
